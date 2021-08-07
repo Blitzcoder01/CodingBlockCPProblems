ee736ca5e1e505e5a7da195f08aff3823fc0ea43
Riya Mathur <51375575+Blitzcoder01@users.noreply.github.com>
2021-08-07 09:14:30 +0530

    Update StringSort.java

--- StringSort.java
+++ StringSort.java
@@ -1,3 +1,24 @@
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Input Format
+First line contains integer N (size of the array) followed by N more integers.
+
+Constraints
+Output Format
+Majority Element of array
+
+Sample Input
+3
+1 1 2
+Sample Output
+1
+Explanation
+Number of 1's in the array is more than 3/2 , Hence majority element is 1
+*/
+
 import java.util.*;
 public class Main {
 	public static void SortString(String[] arr, int n ){
